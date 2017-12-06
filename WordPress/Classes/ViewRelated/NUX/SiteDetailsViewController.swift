47de4e0a001d622fdd9155224071d87ed3aff704
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2017-12-06 16:58:08 -0700

    Adding localized text.

--- WordPress/Classes/ViewRelated/NUX/SiteDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteDetailsViewController.swift
@@ -4,6 +4,9 @@ class SiteDetailsViewController: UIViewController, LoginWithLogoAndHelpViewContr
 
     // MARK: - Properties
 
+    @IBOutlet weak var stepLabel: UILabel!
+    @IBOutlet weak var stepDescrLabel1: UILabel!
+    @IBOutlet weak var stepDescrLabel2: UILabel!
     @IBOutlet weak var siteTitleField: LoginTextField!
     @IBOutlet weak var taglineField: LoginTextField!
     @IBOutlet weak var tagDescrLabel: UILabel!
@@ -17,6 +20,7 @@ class SiteDetailsViewController: UIViewController, LoginWithLogoAndHelpViewContr
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        localizedText()
     }
 
     private func configureView() {
@@ -28,6 +32,16 @@ class SiteDetailsViewController: UIViewController, LoginWithLogoAndHelpViewContr
         tagDescrLabel.textColor = WPStyleGuide.greyDarken20()
         nextButton.isEnabled = false
         siteTitleField.becomeFirstResponder()
+    }
+    
+    private func localizedText() {
+        stepLabel.text = NSLocalizedString("STEP 3 OF 4", comment: "Step for view.")
+        stepDescrLabel1.text = NSLocalizedString("Tell us more about the site you're creating.", comment: "Site details instruction.")
+        stepDescrLabel2.text = NSLocalizedString("What's the title and tagline?", comment: "Site details question.")
+        siteTitleField.placeholder = NSLocalizedString("Add title", comment: "Site title placeholder.")
+        taglineField.placeholder = NSLocalizedString("Optional tagline", comment: "Site tagline placeholder.")
+        tagDescrLabel.text = NSLocalizedString("The tagline is a short line of text shown right below the title in most themes, and acts as site metadata on search engines.", comment: "Tagline description.")
+        nextButton.titleLabel?.text = NSLocalizedString("Next", comment: "Next button title.")
     }
 
     // MARK: - LoginWithLogoAndHelpViewController
